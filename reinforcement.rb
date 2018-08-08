d778e0c71ad639176d380326d690372f3b1d4d03
David Ma <davidma415@gmail.com>
2018-08-08 11:48:00 -0400

    Finished method for exactly 3 cards

--- reinforcement.rb
+++ reinforcement.rb
@@ -1,16 +1,17 @@
 def select_cards(possible_cards, hand)
   counter = 0
-  possible_cards.each do |current_card|
-    if counter < 3
-      print "Do you want to pick up #{current_card}?"
-      answer = gets.chomp
-      if answer.downcase == 'y'
-        hand << current_card
-        counter += 1
+  until counter == 3
+    possible_cards.each do |current_card|
+      if counter < 3
+        print "Do you want to pick up #{current_card}?"
+        answer = gets.chomp
+        if answer.downcase == 'y'
+          hand << current_card
+          counter += 1
+        end
       end
     end
   end
-
   return hand
 end
 
